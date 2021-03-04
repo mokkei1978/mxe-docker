627ec7f8896a2ac11645ee63485d27f6980c8452
Kei Sakamoto <ksakamot@mri-jma.go.jp>
2021-03-04 15:10:22 +0900

    NuSDaS 1.4 をインストールする (環境変数の設定を修正) refs #5268

--- nusdas/run.sh
+++ nusdas/run.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-name=nusdas
-
 docker run -e http_proxy=${HTTP_PROXY} -e HTTPS_PROXY=${HTTP_PROXY} \
-       --rm -it --name ${name} mxe-nusdas-base:latest
+       --rm -it --name nusdas mxe-nusdas-base:latest
 
 exit 0
